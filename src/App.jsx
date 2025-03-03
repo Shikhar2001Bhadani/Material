a958a2f9ff26b52e1f3e5b8a8ae72b135cdcdd91
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-03-03 22:50:24 +0530

    2nd Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Button, Typography } from '@mui/material';
 import React from 'react';
-import Header from './components/header';
+import Header from './components/Header';
 import './App.css';
 import CourseCard from './components/coursecard';
 
